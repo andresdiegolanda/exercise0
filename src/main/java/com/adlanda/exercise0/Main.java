8dee5f2d518ae2357a0ce5719fa71d57d12ea359
Landa, Andres <andres.landa@fmr.com>
2019-01-05 21:57:16 +0000

    step 5 ok

--- src/main/java/com/adlanda/exercise0/Main.java
+++ src/main/java/com/adlanda/exercise0/Main.java
@@ -4,17 +4,25 @@ import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.Map;
 
+/**
+ * Main class, list of fruit should be passed as program arguments e.g: java
+ * Main apple apple orange apple melon melon banana
+ *
+ */
 public class Main {
 
-
-
-	public static void main(String[] args) {
-		Map<String, Long> fruitCounter = ScannerService.getInstance().scan(args);
-		long result = CalculatorService.getInstance().calculate(fruitCounter, DAO.getInstance().getFruits());
-		DecimalFormat df = new DecimalFormat("#.00");
-		System.out.println("input:" + Arrays.toString(args));
-		System.out.println("Aggregated input:" + fruitCounter);
-		System.out.println("Result:" + df.format(result / 100.0));
+	public static void main(String[] args) throws InterruptedException {
+		for (int i = 0; i <= args.length; i++) {
+			String scanned[] = Arrays.copyOfRange(args, 0, i);
+			Map<String, Long> fruitCounter = ScannerService.getInstance().scan(scanned);
+			long result = CalculatorService.getInstance().calculate(fruitCounter, DAO.getInstance().getFruits());
+			DecimalFormat df = new DecimalFormat("#.00");
+			System.out.println("input:" + Arrays.toString(args));
+			System.out.println("scanned:" + Arrays.toString(scanned));
+			System.out.println("Aggregated input:" + fruitCounter);
+			System.out.println("Result:£" + df.format(result / 100.0) + "\n\n");
+			Thread.sleep(1000);
+		}
 	}
 
 }
